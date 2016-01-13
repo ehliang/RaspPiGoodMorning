9bfa192eaa20b51ccb90cc3687e6eb2c5faecffe
Ethan Liang <ethanliang97@gmail.com>
2016-01-12 23:49:01 -0500

    Added Angular interval factory

--- js/pi.js
+++ js/pi.js
@@ -1,6 +1,8 @@
 angular.module('pi',[])
-    .controller('piController', ["$scope", function($scope) {
+    .controller('piController', ["$scope", "$interval", function($scope, $interval) {
      $scope.morning = true;
         $scope.weather = true; 
     
-}]);
+}])
+.directive('MyCurrentTime', ['$interval', 'dateFilter', 
+                            function($interval,dateFilter)])
