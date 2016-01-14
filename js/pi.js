16ff2841f3d5f65c0d8c94d12a3eff65802e2c9d
Ethan Liang <ethanliang97@gmail.com>
2016-01-13 22:14:47 -0500

    Put in wunderground API

--- js/pi.js
+++ js/pi.js
@@ -1,8 +1,25 @@
 angular.module('pi',[])
-    .controller('piController', ["$scope", "$interval", function($scope, $interval) {
+    .controller('piController', ["$scope", "$http", "$interval", function($scope, $http, $interval) {
      $scope.morning = true;
-        $scope.weather = true; 
-    
+    $scope.weather = true; 
+        
+        $scope.method='GET';
+        $scope.url='http://api.wunderground.com/api/d3d8aed6895cf4a5/geolookup/conditions/forecast/q/Canada/Richmond_Hill.json';
+        $scope.abc ="rere";
+        $scope.fetch = function (){
+            $scope.code = null;
+            $scope.response = null; 
+            
+            $http({method: 'GET', url: 'http://api.wunderground.com/api/d3d8aed6895cf4a5/geolookup/conditions/forecast/q/Canada/Richmond_Hill.json'}).success(function(data)
+            {
+                $scope.gets = data; 
+            }
+        });
+                               
+                                
 }])
-.directive('MyCurrentTime', ['$interval', 'dateFilter', 
-                            function($interval,dateFilter)])
+    
+    
+    
+//.directive('MyCurrentTime', ['$interval', 'dateFilter', 
+//                            function($interval,dateFilter)])
